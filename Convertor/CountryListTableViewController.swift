3befb7dbea4b6fd85d96587393821f6ef47a604b
Preetgill786 <57011583+Preetgill786@users.noreply.github.com>
2019-11-11 12:49:52 -0500

    Add files via upload

---
+++ Convertor/CountryListTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CountryList.swift
+//  Convertor
+//
+//  Created by MacStudent on 2019-11-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+protocol CountryListTableViewControllerDelegate: class {
+func itemDetailViewControllerDidCancel(_ controller: CountryListTableViewController)
+    func itemDetailViewController(_ controller: CountryListTableViewController,didFinishAdding item: countryItem)
+    func itemDetailViewController(_ controller: CountryListTableViewController,
+                     didFinishEditing item: countryItem)
+}
+
+class CountryListTableViewController: UITableViewController{
+ var username:String = ""
+    //@IBOutlet weak var usernamelabel: UILabel!
+    let CountryNames = ["USA" , "INDIA" , "CHINA" , "CANADA"]
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+      
+    }
+      override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+//     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return CountryNames.count
+        }
+    
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell
+        {
+        let cell = tableView.dequeueReusableCell(
+        withIdentifier: "countryList",for: indexPath)
+            //cell.textLabel?.text = "hgjghjghj"
+            cell.textLabel?.text = CountryNames[indexPath.row]
+            cell.textLabel?.text = CountryNames[indexPath.row]
+            return cell
+        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.destination is ViewController
+        {
+            let vc = segue.destination as? ViewController
+           
+        }
+    }
+}
---
+++ Convertor/countryItem.swift
@@ -0,0 +1,12 @@
+//
+//  countryItem.swift
+//  Convertor
+//
+//  Created by MacStudent on 2019-11-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class countryItem: NSObject {
+var text = ""
+}
